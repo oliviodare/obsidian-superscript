00a141219aec941ad6ad36a2533601365cbf7fdb
oliviodare <m_bp@posteo.net>
2021-09-20 17:52:01 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -44,22 +44,22 @@ export default class Superscript extends Plugin {
     const tos = editor.posToOffset(editor.getCursor("to")); // to offset
     const len = selectedText.length;
 
-    var beforeText = editor.getRange(Cursor(fos - 3), Cursor(tos - len));
-    var afterText = editor.getRange(Cursor(fos + len), Cursor(tos + 4));
-    var startText = editor.getRange(Cursor(fos), Cursor(fos + 3));
-    var endText = editor.getRange(Cursor(tos - 4), Cursor(tos));
+    var beforeText = editor.getRange(Cursor(fos - 5), Cursor(tos - len));
+    var afterText = editor.getRange(Cursor(fos + len), Cursor(tos + 6));
+    var startText = editor.getRange(Cursor(fos), Cursor(fos + 5));
+    var endText = editor.getRange(Cursor(tos - 6), Cursor(tos));
 
     if (beforeText === "<sup>" && afterText === "</sup>") {
       //=> undo superscript (inside selection)
 
-      editor.setSelection(Cursor(fos - 3), Cursor(tos + 4));
+      editor.setSelection(Cursor(fos - 5), Cursor(tos + 6));
       editor.replaceSelection(`${selectedText}`);
       // re-select
-      editor.setSelection(Cursor(fos - 3), Cursor(tos - 3));
+      editor.setSelection(Cursor(fos - 5), Cursor(tos - 5));
     } else if (startText === "<sup>" && endText === "</sup>") {
       //=> undo superscript (outside selection)
       
-      editor.replaceSelection(editor.getRange(Cursor(fos + 3), Cursor(tos - 4)));
+      editor.replaceSelection(editor.getRange(Cursor(fos + 5), Cursor(tos - 6)));
       // re-select
       editor.setSelection(Cursor(fos), Cursor(tos - 7));
     } else {
@@ -69,12 +69,12 @@ export default class Superscript extends Plugin {
         // console.log("selected");
         editor.replaceSelection(`<sup>${selectedText}</sup>`);
         // re-select
-        editor.setSelection(Cursor(fos + 3), Cursor(tos + 3));
+        editor.setSelection(Cursor(fos + 5), Cursor(tos + 5));
       } else {
         // console.log("not selected");
         editor.replaceSelection(`<sup></sup>`);
         let cursor = editor.getCursor();
-        cursor.ch -= 4;
+        cursor.ch -= 6;
         editor.setCursor(cursor);
       }
     }
